4524476c3141f70ac92c9625811dc5ea73dc82eb
Ken Tang <kenftang@gmail.com>
2023-07-30 19:37:58 +0100

    Add GameProperties class
    GameProperties contains a couple of the
    common delegate classes that I tend to use.

---
+++ Assets/Scripts/GameProperties.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnSomeEvent();
+public delegate void SendInt(int value);
+public delegate void SendFloat(float value);
+public delegate void SendString(string value);
+
+public class GameProperties
+{
+    
+}
